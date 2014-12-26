c3ecf09fdea6cdb98ca3bb9ca0401700543cae10
Lee <jessy1092@gmail.com>
2014-12-26 23:15:38 +0800

    Add Title component for Accordion.
    Signed-off-by: Lee <jessy1092@gmail.com>

---
+++ src/commons/title.jsx
@@ -0,0 +1,31 @@
+"use strict";
+module.exports = function (React) {
+
+  var ClassGenerator = require('../mixins/classGenerator.js')(React);
+  var StateSelector  = require('../mixins/stateSelector.js')(React);
+  var Unit           = require('../commons/unit.jsx')(React);
+
+  var defaultClassName = 'title';
+
+  var Title = React.createClass({
+
+    mixins: [ClassGenerator, StateSelector],
+
+    render: function () {
+
+      var {className, type, color, active, ...other} = this.props;
+
+      return (
+        <Unit {...other}
+          className={this.getClassName(defaultClassName)}
+          type="div"
+          color="null"
+          active={this.getActive()}>
+          {this.props.children}
+        </Unit>
+      );
+    }
+  });
+
+  return Title;
+}
