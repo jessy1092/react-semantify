7cddced6d12a984e70fd67b36ee6327e1289e1bf
Lee <jessy1092@gmail.com>
2015-01-03 00:43:33 +0800

    Use "npm install miller-rabin@1.1.1 --save-dev" for pass browserify install failed.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- test/__tests__/elements/Button-test.js
+++ test/__tests__/elements/Button-test.js
@@ -1,4 +1,4 @@
-/** @jsx React.DOM */
+"use strict";
 
 jest.dontMock('../../../src/index.js');
 
@@ -7,12 +7,28 @@ var TestUtils = require('react/lib/ReactTestUtils');
 var Button    = require('../../../src/index.js').Button;
 
 describe('Button', function () {
-  it('should have .ui.button class', function () {
+  it('should have .ui.button class by default', function () {
     var instance = TestUtils.renderIntoDocument(
       <Button></Button>
     );
 
     expect(instance.getDOMNode().className).toMatch('ui');
     expect(instance.getDOMNode().className).toMatch('button');
+  });
+
+  it('should have blue class with color is blue ', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Button color="blue"></Button>
+    );
+
+    expect(instance.getDOMNode().className).toMatch('blue');
+  });
+
+  it('should have disabled class with disabled is true', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Button disabled={true}></Button>
+    );
+
+    expect(instance.getDOMNode().className).toMatch('disabled');
   });
 });
