d411e15b9d54d5951d6abd25d190483096750a7e
Lee <jessy1092@gmail.com>
2015-01-03 14:20:13 +0800

    Fix the label bug: onClick Function.

---
+++ test/__tests__/elements/Label-test.js
@@ -0,0 +1,66 @@
+"use strict";
+
+jest.dontMock('../../../src/index.js');
+
+var React     = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+var Label     = require('../../../src/index.js').Label;
+
+describe('Label', function () {
+  it('should have .ui.label class by default', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Label></Label>
+    );
+
+    expect(instance.getDOMNode().className).toMatch('ui');
+    expect(instance.getDOMNode().className).toMatch('label');
+  });
+
+  it('should have child by default', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Label>123</Label>
+    );
+
+    expect(instance.getDOMNode().textContent).toMatch('123');
+  });
+
+
+  it('should have custom class with custom className', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Label className="custom"></Label>
+    );
+
+    expect(instance.getDOMNode().className).toMatch('custom');
+  });
+
+  it('should have blue class with color is blue', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Label color="blue"></Label>
+    );
+
+    expect(instance.getDOMNode().className).toMatch('blue');
+  });
+
+  it('should be <div> if type is div', function () {
+    var instance = TestUtils.renderIntoDocument(
+      <Label type="div"></Label>
+    );
+
+    expect(TestUtils.findRenderedDOMComponentWithTag(instance, 'div')).toBeDefined();
+  });
+
+  it('should call onClick callback when icon click', function (done) {
+
+    var clickOp = function () {
+      done();
+    };
+
+    var instance = TestUtils.renderIntoDocument(
+      <Label onClick={clickOp}></Label>
+    );
+
+    var label = TestUtils.findRenderedDOMComponentWithClass(instance, 'label');
+
+    TestUtils.Simulate.click(label);
+  });
+});
