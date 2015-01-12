67b26a2d2be29aea4ea6b8802e805c4d5c3dc7c7
Lee <jessy1092@gmail.com>
2015-01-13 01:55:04 +0800

    Create ButtonPage.jsx
    Signed-off-by: Lee <jessy1092@gmail.com>

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -7,13 +7,10 @@ var React        = require('react/addons');
 var Sidebar      = require('../../src/components/Sidebar.jsx');
 var RouteActions = require('../../src/actions/RouteActions.js');
 var App          = require('../../src/App.jsx');
-// var IndexPage = require('../../src/pages/IndexPage.jsx');
 
 var pathName = location.hash.substring(2);
 
 React.render(<Sidebar />, document.getElementsByClassName('ui sidebar')[0]);
 React.render(<App />, document.getElementsByClassName('pusher')[0]);
-
-// React.render(<IndexPage />, document.getElementsByClassName('pusher')[0]);
 
 RouteActions.updatePath(pathName);
--- src/pages/ButtonPage.jsx
+++ src/pages/ButtonPage.jsx
@@ -3,15 +3,30 @@ var React     = require('react/addons');
 var Semantify = require('react-semantify');
 var SidebarButton = require('../components/SidebarButton.jsx');
 
-var {Button} = Semantify;
+var {Button, Divider, Header, Segment} = Semantify;
 
 module.exports = React.createClass({
 
   render: function () {
     return (
       <div>
+        <Segment className="header">
+          <div className="container">
+            <Header className="huge">
+              Button
+              <Header className="sub">A button indicates a possible user action</Header>
+            </Header>
+            <Divider />
+          </div>
+        </Segment>
+        <div className="container">
+          <Header className="large dividing">Basic</Header>
+          <Header className="small" >
+            Button
+            <Header className="sub">A standard button</Header>
+          </Header>
+        </div>
         <SidebarButton />
-        <Button className="inverted" color='blue'>ButtonPage</Button>
       </div>
     );
   }
